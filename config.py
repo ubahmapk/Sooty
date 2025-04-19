1138747ff5071e7f26bd66d6fb1d1f5b380ebca4
Jon Mark Allen <ubahmapk@gmail.com>
2025-04-19 15:02:49 -0500

    Use functools.cache to cache results of configvars for future calls

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+from functools import cache
 from pathlib import Path
 
 import strictyaml
@@ -6,7 +7,18 @@ import strictyaml
 versionNo = "1.3.2"
 
 
+@cache
 def get_config_vars() -> OrderedDict:
+    """Read and return the configuration variables from the config.yaml file.
+
+    Reads the config.yaml file and returns the configuration variables as an OrderedDict.
+    If the file is not found or is not valid YAML, an empty OrderedDict is returned.
+
+    Uses functools.cache to cache the result for performance.
+    This way, any module can call this function to get the configuration variables,
+    but the file only needs to be read once.
+    """
+
     config_file: Path = Path("config.yaml")
 
     try:
