7d09e547373909f13191ff4c48afaed566e6cbed
Jon Mark Allen <ubahmapk@gmail.com>
2025-04-19 13:04:10 -0500

    extracted config and urlscan to separate file

--- Sooty.py
+++ Sooty.py
@@ -11,12 +11,12 @@ import os
 import re
 import time
 import tkinter
-from pathlib import Path
+from collections import OrderedDict
 
 import requests
-import strictyaml
 from rich import print as rprint
 
+from config import get_config_vars
 from decoder import decoder_menu
 from dns_menu import dns_menu
 from extras import extras_menu
@@ -28,15 +28,6 @@ try:
     import win32com.client
 except ModuleNotFoundError:
     print("Cant install Win32com package")
-
-versionNo = "1.3.2"
-
-configfile: Path = Path("config.yaml")
-try:
-    with configfile.open() as f:
-        configvars = strictyaml.load(f.read())
-except FileNotFoundError:
-    print("Config.yaml not found. Check the example config file and rename to 'config.yaml'.")
 
 linksFoundList = []
 linksRatingList = []
@@ -62,62 +53,6 @@ def phishingSwitch(choice):
 def titleLogo():
     TitleOpen.titleOpen()
     os.system("cls||clear")
-
-
-def urlscanio():
-    print("\n --------------------------------- ")
-    print("\n        U R L S C A N . I O        ")
-    print("\n --------------------------------- ")
-    url_to_scan = str(input("\nEnter url: ").strip())
-
-    type_prompt = str(input('\nSet scan visibility to Public? \nType "1" for Public or "2" for Private: '))
-    scan_type = "public" if type_prompt == "1" else "private"
-
-    headers = {
-        "Content-Type": "application/json",
-        "API-Key": configvars.data["URLSCAN_IO_KEY"],
-    }
-
-    response = requests.post(
-        "https://urlscan.io/api/v1/scan/", headers=headers, data='{"url": "%s", "%s": "on"}' % (url_to_scan, scan_type)
-    ).json()
-
-    if "successful" not in response["message"]:
-        print(response["message"])
-        return
-
-    print("\nNow scanning %s. Check back in around 1 minute." % url_to_scan)
-    uuid_variable = str(response["uuid"])  # uuid, this is the factor that identifies the scan
-    time.sleep(
-        45
-    )  # sleep for 45 seconds. The scan takes awhile, if we try to retrieve the scan too soon, it will return an error.
-    scan_results = requests.get(
-        "https://urlscan.io/api/v1/result/%s/" % uuid_variable
-    ).json()  # retrieving the scan using the uuid for this scan
-
-    task_url = scan_results["task"]["url"]
-    verdicts_overall_score = scan_results["verdicts"]["overall"]["score"]
-    verdicts_overall_malicious = scan_results["verdicts"]["overall"]["malicious"]
-    task_report_URL = scan_results["task"]["reportURL"]
-
-    print("\nurlscan.io Report:")
-    print("\nURL: " + task_url)
-    print("\nOverall Verdict: " + str(verdicts_overall_score))
-    print("Malicious: " + str(verdicts_overall_malicious))
-    print("urlscan.io: " + str(scan_results["verdicts"]["urlscan"]["score"]))
-    if scan_results["verdicts"]["urlscan"]["malicious"]:
-        print("Malicious: " + str(scan_results["verdicts"]["urlscan"]["malicious"]))  # True
-    if scan_results["verdicts"]["urlscan"]["categories"]:
-        print("Categories: ")
-    for line in scan_results["verdicts"]["urlscan"]["categories"]:
-        print("\t" + str(line))  # phishing
-    for line in scan_results["verdicts"]["engines"]["verdicts"]:
-        print(str(line["engine"]) + " score: " + str(line["score"]))  # googlesafebrowsing
-        print("Categories: ")
-        for item in line["categories"]:
-            print("\t" + item)  # social_engineering
-    print("\nSee full report for more details: " + str(task_report_URL))
-    print("")
 
 
 def phishingMenu():
@@ -547,4 +482,5 @@ def mainMenu() -> None:
 
 if __name__ == "__main__":
     titleLogo()
+    configvars: OrderedDict = get_config_vars()
     mainMenu()
